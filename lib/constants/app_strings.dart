32dd15faebd2e822ad63872ba583a80dd24fa4b9
Emran Khandaker Evan <evanemran007@gmail.com>
2024-01-29 00:22:37 +0600

    live url added

--- lib/constants/app_strings.dart
+++ lib/constants/app_strings.dart
@@ -2,7 +2,8 @@ class AppStrings {
   static const String appName = "Avenger Art";
   static const String errorMsg = "An Error Occurred!";
 
-  static const String baseUrl = "http://192.168.0.238:8100/";
+  // static const String baseUrl = "http://192.168.0.238:8100/"; //local
+  static const String baseUrl = "http://128.199.180.160:8100/";  //live
   static const String allAvengers = "all-avengers";
   static const String randomAvengers = "random-avenger";
 }
